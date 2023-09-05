c5ab0363d9026e45d41e78f23067c21f232f2c39
ARAFAT ZAYED LAM <144141842+LAM-47@users.noreply.github.com>
2023-09-05 20:43:07 +0600

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+    float radius,area;
+    printf("Enter Circle radius:");
+    scanf("%f",&radius);
+    area=3.14159*radius*radius;
+    printf("Area Of a Circle: %f",area);
+    return 0;
+}
